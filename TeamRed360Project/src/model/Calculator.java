10a6b9895af14c528e26a06220473344579e28a3
Taylor <triccetti@cettiworks.com>
2017-05-30 17:57:40 -0700

    updating

--- TeamRed360Project/src/model/Calculator.java
+++ TeamRed360Project/src/model/Calculator.java
@@ -1,7 +1,5 @@
 package model;
 
-import java.util.regex.*;
-
 /**
  * This is the back end class to the calculator method
  * 
@@ -36,7 +34,7 @@ public class Calculator {
 	 * @return returns the finished equation
 	 */
 	public static double eval(final String stringEq) {
-		System.out.println(stringEq.length());
+
 		if(stringEq.length() > 0){
 		
 			return new Object() {
@@ -46,79 +44,7 @@ public class Calculator {
 					ch = (++pos < stringEq.length()) ? stringEq.charAt(pos) : -1;
 				}
 
-<<<<<<< HEAD
-	        boolean eat(int charToEat) {
-	            while (ch == ' ') nextChar();
-	            if (ch == charToEat) {
-	                nextChar();
-	                return true;
-	            }
-	            return false;
-	        }
-
-	        double parse() {
-	            nextChar();
-	            double x = parseExpression();
-	            if (pos < stringEq.length()) {
-	            	throw new RuntimeException("Unexpected: " + (char)ch);
-	            }
-	            return x;
-	        }
-	        // Grammar:
-	        // expression = term | expression `+` term | expression `-` term
-	        // term = factor | term `*` factor | term `/` factor
-	        // factor = `+` factor | `-` factor | `(` expression `)`
-	        //        | number | functionName factor | factor `^` factor
-
-	        double parseExpression() {
-	            double x = parseTerm();
-	            for (;;) {
-	                if      (eat('+')) x += parseTerm(); // addition
-	                else if (eat('-')) x -= parseTerm(); // subtraction
-	                else return x;
-	            }
-	        }
-	        
-	        double parseTerm() {
-	            double x = parseFactor();
-	            for (;;) {
-	                if      (eat('*')) x *= parseFactor(); // multiplication
-	                else if (eat('/')) x /= parseFactor(); // division
-	                else return x;
-	            }
-	        }
-
-	        double parseFactor() {
-	            if (eat('+')) return parseFactor(); // unary plus
-	            if (eat('-')) return -parseFactor(); // unary minus
-
-	            double x;
-	            int startPos = this.pos;
-	            if (eat('(')) { // parentheses
-	                x = parseExpression();
-	                eat(')');
-	            } else if ((ch >= '0' && ch <= '9') || ch == '.') { // numbers
-	                while ((ch >= '0' && ch <= '9') || ch == '.') nextChar();
-	                x = Double.parseDouble(stringEq.substring(startPos, this.pos));
-	            } else if (ch >= 'a' && ch <= 'z') { // functions
-	                while (ch >= 'a' && ch <= 'z') nextChar();
-	                String func = stringEq.substring(startPos, this.pos);
-	                x = parseFactor();
-	                if (func.equals("sqrt")) x = Math.sqrt(x);
-	                else if (func.equals("sin")) x = Math.sin(Math.toRadians(x));
-	                else if (func.equals("cos")) x = Math.cos(Math.toRadians(x));
-	                else if (func.equals("tan")) x = Math.tan(Math.toRadians(x));
-	                else throw new RuntimeException("Unknown function: " + func);
-	            } else {
-	                throw new RuntimeException("Unexpected: " + (char)ch);
-	            }
-	            if (eat('^')) x = Math.pow(x, parseFactor()); // exponentiation
-
-	            return x;
-	        }
-	    }.parse();
-	}
-=======
+	       
 		        boolean eat(int charToEat) {
 		            while (ch == ' ') nextChar();
 		            if (ch == charToEat) {
@@ -182,14 +108,12 @@ public class Calculator {
 		            } else {
 		                throw new RuntimeException("Unexpected: " + (char)ch);
 		            }
-		            if (eat('^')) x = Math.pow(x, parseFactor()); // exponentiation
->>>>>>> 52a9cc80ea7ff88564822c05fc3fc8d00ecf2001
-	
+		            if (eat('^')) x = Math.pow(x, parseFactor()); // exponentiation 
+		            
 		            return x;
 		        }
 		    }.parse();
-		}
-		else{
+		} else {
 			return 0.0;
 		}
 		
